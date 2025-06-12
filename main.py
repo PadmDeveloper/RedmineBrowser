ac0f99a1a351d441e48d459aff9aaba17b8608f6
PadmDeveloper <acmepadm001@gmail.com>
2025-06-12 12:48:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ class RedmineAutomator:
             # Launch browser in headless mode using system Chromium
             browser = await p.chromium.launch(
                 headless=True,
-                executable_path='/nix/store/zi4f80l169xlmivz8vja8wlphq74qqk0-chromium-125.0.6422.141/bin/chromium',
-                args=['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+                args=['--no-sandbox']
             )
             
             try:
