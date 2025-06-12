7faddc95d2e21bb67f117352bfb1895352075fe2
PadmDeveloper <acmepadm001@gmail.com>
2025-06-12 15:56:08 +0530

    Create install_deps.sh

---
+++ install_deps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+apt-get update && \
+apt-get install -y \
+  libgtk-4-1 \
+  libgraphene-1.0-0 \
+  libgstgl-1.0-0 \
+  libgstcodecparsers-1.0-0 \
+  libavif15 \
+  libenchant-2-2 \
+  libsecret-1-0 \
+  libmanette-0.2-0 \
+  libgles2 \
+  libnss3 \
+  libxss1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libgtk-3-0 \
+  libdrm2 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libcups2 \
+  libgbm1 \
+  libpango-1.0-0 \
+  libpangocairo-1.0-0 \
+  libatk1.0-0 \
+  libatspi2.0-0 \
+  libx11-xcb1 \
+  libxshmfence1 \
+  libxrender1 \
+  libxcb1 \
+  libxext6 \
+  libgl1
